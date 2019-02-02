a4cb53963627332bee2e246c8a6de641516c16f3
Kenneth <kenneth@Kenneths-iMac-Pro.local>
2019-02-01 20:21:46 -0700

    First draft (WIP)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
+
+gem 'bootstrap', '~> 4.2.1'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+   
+  root :to => 'homes#index'
+
+  get '/', to: 'homes#index'
+
+  get 'homes', to: 'homes#index'
+
+  get '/homes', to: 'homes#index'
+
+  get 'homes/index', to: 'homes#index'
+
+  resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
